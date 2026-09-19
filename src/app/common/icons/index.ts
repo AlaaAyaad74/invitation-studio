f08fde1c858d24116c8ab2520243d42357943a39
AlaaAyaad74 <alaaayad1@outlook.com>
2026-09-19 20:52:05 +0300

    Remove unused SVG icon files and related TypeScript components to streamline the project and improve maintainability.

--- src/app/common/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CalendarIcon } from "./CalendarIcon";
-export { default as GlobeIcon } from "./GlobeIcon";
-export { default as InstagramIcon } from "./InstagramIcon";
-export { default as MusicNoteIcon } from "./MusicNoteIcon";
-export { default as PauseIcon } from "./PauseIcon";
-export { default as WhatsAppIcon } from "./WhatsAppIcon";
